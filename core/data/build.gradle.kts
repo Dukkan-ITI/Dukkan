69cde79fb3bba89aceb210e65f1521fb1d4c447f
Ahlam <129538648+Ahlamgomaa@users.noreply.github.com>
2026-06-27 16:18:37 +0300

    SetUp DataBase

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.library)
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -29,4 +30,12 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
+
+    val room_version = "2.8.4"
+    // Room
+    implementation("androidx.room:room-runtime:$room_version")
+    ksp("androidx.room:room-compiler:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
+
+
 }
