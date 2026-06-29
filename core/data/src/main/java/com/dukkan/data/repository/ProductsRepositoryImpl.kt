8fed3ae0c3d4649d0b489b83e870c6229f877131
Mohannad El-Sayeh <eng.mohannadelsayeh@gmail.com>
2026-06-29 09:18:40 +0300

    Map remote product data to domain model in ProductsRepositoryImpl

--- core/data/src/main/java/com/dukkan/data/repository/ProductsRepositoryImpl.kt
+++ core/data/src/main/java/com/dukkan/data/repository/ProductsRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.dukkan.data.repository
 
+import com.dukkan.data.mapper.toDomainModel
 import com.dukkan.data.source.remote.apollo.ProductsDataSource
 import com.msayeh.domain.model.Product
 import com.msayeh.domain.repository.ProductsRepository
@@ -8,7 +9,7 @@ class ProductsRepositoryImpl(private val productsDataSource: ProductsDataSource)
     override suspend fun getProductById(productId: String): Result<Product> {
         productsDataSource.getProductById(productId).also {
             return if (it != null) {
-                Result.success(it)
+                Result.success(it.toDomainModel())
             } else {
                 Result.failure(Exception("Unknown error occurred"))
             }
