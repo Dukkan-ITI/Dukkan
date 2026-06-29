d70bf737cc0da89a783888382b39791b5fde017c
hazemkora660@gmail.com <hazemkora660@gmail.com>
2026-06-29 17:59:55 +0300

    fix(nav): remove MainActivity overlay and add dummy Home screen

--- app/src/main/java/com/dukkan/app/AppNavGraph.kt
+++ app/src/main/java/com/dukkan/app/AppNavGraph.kt
@@ -9,6 +9,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.dukkan.navigation.Screen
+import com.dukkan.onboarding.view.OnboardingView
 
 @Composable
 fun AppNavGraph(
@@ -17,21 +18,63 @@ fun AppNavGraph(
 ) {
     NavHost(
         navController = navController,
-        startDestination = Screen.Home.route,
+        startDestination = Screen.Onboarding.route,
         modifier = modifier
     ) {
 
-        composable(route = Screen.Login.route) {}
+        composable(route = Screen.Onboarding.route) {
+            OnboardingView(
+                onFinish = {
+                    navController.navigate(Screen.Login.route) {
+                        popUpTo(Screen.Onboarding.route) { inclusive = true }
+                    }
+                },
+                onSignIn = {
+                    navController.navigate(Screen.Login.route) {
+                        popUpTo(Screen.Onboarding.route) { inclusive = true }
+                    }
+                }
+            )
+        }
 
-        composable(route = Screen.SignUp.route) {}
+        composable(route = Screen.Login.route) {
+            com.darkzoom.auth.login.view.LoginScreen(
+                onNavigateToRegister = { navController.navigate(Screen.SignUp.route) },
+                onLoginSuccess = {
+                    navController.navigate(Screen.Home.route) {
+                        popUpTo(Screen.Login.route) { inclusive = true }
+                    }
+                },
+                onContinueAsGuest = {
+                    navController.navigate(Screen.Home.route) {
+                        popUpTo(Screen.Login.route) { inclusive = true }
+                    }
+                }
+            )
+        }
+
+        composable(route = Screen.SignUp.route) {
+            com.darkzoom.auth.register.view.RegisterScreen(
+                onNavigateToLogin = {
+                    navController.navigate(Screen.Login.route) {
+                        popUpTo(Screen.SignUp.route) { inclusive = true }
+                    }
+                },
+                onRegisterSuccess = {
+                    navController.navigate(Screen.Home.route) {
+                        popUpTo(Screen.SignUp.route) { inclusive = true }
+                    }
+                },
+                onContinueAsGuest = {
+                    navController.navigate(Screen.Home.route) {
+                        popUpTo(Screen.SignUp.route) { inclusive = true }
+                    }
+                }
+            )
+        }
 
         composable(route = Screen.Home.route) {
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
-            ) {
-
-            }
+            androidx.compose.material3.Text("Dummy Screen")
         }
 
         composable(route = Screen.Search.route) {}
