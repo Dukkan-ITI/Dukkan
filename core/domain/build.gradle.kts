18bd6ea2e7fd387bd2d496b04ee9aff797025f88
Ahlam <129538648+Ahlamgomaa@users.noreply.github.com>
2026-06-29 17:26:52 +0300

    enhanced lib

--- core/domain/build.gradle.kts
+++ core/domain/build.gradle.kts
@@ -14,6 +14,6 @@ kotlin {
 dependencies {
     implementation(libs.javax.inject)
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation(libs.kotlinx.coroutines.core)
 
 }
--- features/favorites/build.gradle.kts
+++ features/favorites/build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
 
     implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.2")
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     implementation(libs.androidx.compose.material.icons)
