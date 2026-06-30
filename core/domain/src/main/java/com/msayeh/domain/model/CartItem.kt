3305025d4e45f5c4e246e6bcd32616368f849912
Ahlam <129538648+Ahlamgomaa@users.noreply.github.com>
2026-07-01 00:20:41 +0300

    setup domain layer for cart and built dependency injection

---
+++ core/domain/src/main/java/com/msayeh/domain/model/CartItem.kt
@@ -0,0 +1,11 @@
+package com.msayeh.domain.model
+
+data class CartItem(
+    val id: String,
+    val title: String,
+    val imageUrl: String,
+    val price: String,
+    val currencyCode: String,
+    val size: String,
+    val quantity: Int
+)
