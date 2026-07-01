8fe87b61a31a50448c1123b809b4c298dac485d3
Thaowpsta <thaowpstasaiid@gmail.com>
2026-07-01 18:52:47 +0300

    refactor: move-product-card-to-design-system

--- core/design_system/build.gradle.kts
+++ core/design_system/build.gradle.kts
@@ -32,9 +32,11 @@ dependencies {
     implementation(libs.androidx.compose.ui.text.google.fonts)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
     implementation(platform(libs.androidx.compose.bom.v20240200))
+    implementation(libs.coil.compose)
 }
