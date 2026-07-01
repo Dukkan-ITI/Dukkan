5579e1cecb3e77d672a4c95da68e7c7deecd821f
Mohannad El-Sayeh <eng.mohannadelsayeh@gmail.com>
2026-07-01 17:06:36 +0300

    Clean up imports and remove unused navigation dependency in home feature

--- features/home/build.gradle.kts
+++ features/home/build.gradle.kts
@@ -9,7 +9,6 @@ android {
 dependencies {
     implementation(project(":core:design_system"))
     implementation(project(":core:domain"))
-    implementation(project(":core:navigation"))
 
     implementation(libs.androidx.compose.material.icons.core)
     implementation(libs.coil.compose)
