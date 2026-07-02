e04627704267082690043cb07bfde85e61d24524
hazemkora660@gmail.com <hazemkora660@gmail.com>
2026-07-02 05:01:39 +0300

    feat(domain): add search repository interface

---
+++ core/domain/src/main/java/com/msayeh/domain/repository/SearchRepository.kt
@@ -0,0 +1,14 @@
+package com.msayeh.domain.repository
+
+import com.msayeh.domain.model.PredictiveSearchResult
+import com.msayeh.domain.model.SearchResult
+
+interface SearchRepository {
+    suspend fun searchProducts(
+        query: String,
+        first: Int = 20,
+        after: String? = null
+    ): Result<SearchResult>
+
+    suspend fun predictiveSearch(query: String): Result<PredictiveSearchResult>
+}
