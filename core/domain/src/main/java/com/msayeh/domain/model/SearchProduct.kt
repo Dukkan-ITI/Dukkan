c72416a4b89387cc476d9e72f30f20feded416ae
hazemkora660@gmail.com <hazemkora660@gmail.com>
2026-07-02 05:01:39 +0300

    feat(domain): add search models

---
+++ core/domain/src/main/java/com/msayeh/domain/model/SearchProduct.kt
@@ -0,0 +1,12 @@
+package com.msayeh.domain.model
+
+data class SearchProduct(
+    val id: String,
+    val title: String,
+    val vendor: String,
+    val availableForSale: Boolean,
+    val price: Money,
+    val imageUrl: String?,
+    val imageAltText: String?,
+    val variants: List<SearchProductVariant>
+)
