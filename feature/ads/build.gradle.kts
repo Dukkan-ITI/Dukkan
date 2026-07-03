5558ef0b5a4f307b5b7bc814f301bd66d69fce59
Ahlam <129538648+Ahlamgomaa@users.noreply.github.com>
2026-07-03 18:57:41 +0300

    added strings-ar

--- feature/ads/build.gradle.kts
+++ feature/ads/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.compose)
+    id("dukkan.feature")
     id("dukkan.hilt")
 }
 
@@ -37,26 +38,20 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.hilt.navigation.compose)
-    implementation(project(":core:domain"))
     implementation(libs.androidx.compose.material.icons.extended)
-
     implementation(libs.androidx.compose.foundation.layout)
     implementation(libs.androidx.ui.graphics)
-
     implementation(platform(libs.androidx.compose.bom))
-
-
     implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.glance)
-
-
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
-
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
+
+
 
 }
